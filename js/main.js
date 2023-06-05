5a1e40cfa7e32b06c2851a0abcd10e1abe5acf9b
Marek <sincereakram@gmail.com>
2023-06-05 16:02:31 +0300

    Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const divDOM = document.querySelector('div');
+const data = fetch('https://front-end-by-rimantas.github.io/44-grupe-portfolio/css/components/services.css')
+.then ((Response) => {
+    return Response.text();
+})
+.then((data) => {
+    for (const user of data) {
+        console.log(user);
+    }
+});
